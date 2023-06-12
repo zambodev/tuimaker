815e409668f48d3c84b0f4085949c92f97c25753
zambodev <thomasz01@outlook.it>
2023-06-12 16:59:42 +0200

    New test UI

--- test/main.cpp
+++ test/main.cpp
@@ -3,25 +3,40 @@
 #include <array>
 #include <tui.hpp>
 
-void ciao(void)
+
+void notif(Tui *tui, int *show)
 {
-	wprintf(L"\x1b[30;30HCIAO");
-	fflush(stdout);
+	if(*show == 0)
+	{
+		tui->draw_box("notification");
+		tui->write_box("notification", {"Nothing to see"});
+		*show = 1;
+	}
+	else
+	{
+		tui->clear_box("notification");
+		*show = 0;
+	}
+
+	tui->refresh();
 }
 
 int main()
 {
 	Tui *tui;
 	tui = tui->get_instance("Test App");
+	
+	int show = 0;
 
 	tui->create_box("notification", tui->get_size()[0] - 30, tui->get_size()[1] - 20, tui->get_size()[0], tui->get_size()[1], "Notifications");
 
-	tui->create_selec("options", 2, tui->get_size()[1] - 1, 0, {"Notifcation", "GINO"}, {[tui](){tui->draw_box("notification"); tui->write_box("notification", {"Nothing to see"}); tui->refresh();}, ciao});
+	tui->create_selec("options", 2, tui->get_size()[1] - 1, 0, {"Notification"}, {[tui, &show](){notif(tui, &show);}});
 	tui->draw_selec("options");
 	tui->refresh();
 
-
 	tui->input_selec("options");
+	tui->input_selec("options");
+
 	
 	while(1);
 }
