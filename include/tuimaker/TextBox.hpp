7a140a6cc1716f10cfe498036509af3375ffcb72
zambodev <thomaszambelli01@gmail.com>
2025-07-19 22:51:11 +0200

    Better delete

--- include/tuimaker/TextBox.hpp
+++ include/tuimaker/TextBox.hpp
@@ -29,12 +29,28 @@ namespace tmk
             {
             // Backspace
             case 127:
-                buffer_[(cur_y_ * size_.width) + cur_x_ - 1] = ' ';
+                if (cur_x_ == 1 && cur_y_ == 1)
+                {
+                    return;
+                }
+                else if (cur_x_ == 1 && cur_y_ > 1)
+                {
+                    --cur_y_;
+                    cur_x_ = (is_line_empty(cur_y_) ? 1 : get_last_char_idx(cur_y_));
+
+                    return;
+                }
+
                 --cur_x_;
                 ++wordBeginIdx;
+
+                buffer_[(cur_y_ * size_.width) + cur_x_] = U_SPACE;
+
+                return;
                 break;
             case ' ':
                 wordBeginIdx = 1;
+                c = U_SPACE;
                 break;
             case '\n':
                 ++cur_y_;
@@ -68,7 +84,7 @@ namespace tmk
                 {
                     buffer_[cur_y_ * size_.width + cursorTmp] =
                         buffer_[((cur_y_ - 1) * size_.width) + cur_x_ + wordBeginIdx];
-                    buffer_[((cur_y_ - 1) * size_.width) + cur_x_ + wordBeginIdx] = ' ';
+                    buffer_[((cur_y_ - 1) * size_.width) + cur_x_ + wordBeginIdx] = U_SPACE;
 
                     ++cursorTmp;
                 }
@@ -92,7 +108,7 @@ namespace tmk
                     buffer_[y * size_.width + x] = buffer_[(y + 1) * size_.width + x];
 
                     if (y == (size_.height - 3))
-                        buffer_[(y + 1) * size_.width + x] = ' ';
+                        buffer_[(y + 1) * size_.width + x] = U_SPACE;
                 }
             }
 
@@ -100,6 +116,24 @@ namespace tmk
 
             if (reset_cur_x)
                 cur_x_ = 1;
+        }
+
+        bool is_line_empty(uint64_t line)
+        {
+            for (uint64_t x = 1; x < size_.width - 1; ++x)
+                if (buffer_[line * size_.width + x] != U_SPACE)
+                    return false;
+
+            return true;
+        }
+
+        uint64_t get_last_char_idx(uint64_t line)
+        {
+            for (uint64_t x = size_.width - 2; x >= 1; --x)
+                if (buffer_[line * size_.width + x] != U_SPACE)
+                    return x + 1;
+
+            return 1;
         }
     };
 }
