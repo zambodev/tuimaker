30049d2f161a51125e2afa905e0d5762def9c500
zambodev <thomaszambelli01@gmail.com>
2025-07-19 16:00:45 +0200

    Scrolling textbox

--- tests/main.cpp
+++ tests/main.cpp
@@ -14,6 +14,10 @@ int main(void)
     auto rootWindow = wm->create_window<tmk::Window>({0, 0, tmk::Utils::get_term_width(), tmk::Utils::get_term_height()}, 0);
     auto w2 = wm->create_window<tmk::TextBox>({10, 5, 60, 20}, rootWindow->get_id());
 
-    w2->write("ciao\nciao2");
-    wm->render(rootWindow->get_id());
+    for (size_t i = 0; i < 100; ++i)
+    {
+        w2->write(std::format("ciao {}\n", i));
+        wm->render(rootWindow->get_id());
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+    }
 }
